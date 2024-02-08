55d192b79694e6d0f039978db10579a241b0d16a
Peter Donath <donath.peter@gmail.com>
2024-02-08 23:20:21 +0100

    fix metric naming

--- src/main/java/net/konzol/easunjava/application/metrics/InverterMetrics.java
+++ src/main/java/net/konzol/easunjava/application/metrics/InverterMetrics.java
@@ -21,22 +21,22 @@ public class InverterMetrics {
     public void updateDeviceStatusMetrics(DeviceStatus deviceStatus) {
         Inverter inverter = deviceStatus.getInverter();
 
-        registerGauge(inverter, "grid_voltage", deviceStatus, DeviceStatus::getGridVoltage);
-        registerGauge(inverter, "grid_frequency", deviceStatus, DeviceStatus::getGridFrequency);
-        registerGauge(inverter, "output_voltage", deviceStatus, DeviceStatus::getOutputVoltage);
-        registerGauge(inverter, "output_frequency", deviceStatus, DeviceStatus::getOutputFrequency);
-        registerGauge(inverter, "output_apparent_power", deviceStatus, DeviceStatus::getOutputApparentPower);
-        registerGauge(inverter, "output_active_power", deviceStatus, DeviceStatus::getOutputActivePower);
-        registerGauge(inverter, "bus_voltage", deviceStatus, DeviceStatus::getBusVoltage);
-        registerGauge(inverter, "output_load_percent", deviceStatus, DeviceStatus::getOutputLoadPercent);
-        registerGauge(inverter, "battery_voltage", deviceStatus, DeviceStatus::getBatteryVoltage);
-        registerGauge(inverter, "battery_charge_current", deviceStatus, DeviceStatus::getBatteryChargeCurrent);
-        registerGauge(inverter, "battery_state_of_charge", deviceStatus, DeviceStatus::getBatteryStateOfCharge);
-        registerGauge(inverter, "inverter_heat_sink_temp", deviceStatus, DeviceStatus::getInverterHeatSinkTemperature);
-        registerGauge(inverter, "solar_input_current", deviceStatus, DeviceStatus::getSolarInputCurrent);
-        registerGauge(inverter, "solar_input_voltage", deviceStatus, DeviceStatus::getSolarInputVoltage);
-        registerGauge(inverter, "battery_voltage_scc", deviceStatus, DeviceStatus::getBatteryVoltageScc);
-        registerGauge(inverter, "battery_discharge_current", deviceStatus, DeviceStatus::getBatteryDischargeCurrent);
+        registerGauge(inverter, "easun_grid_voltage", deviceStatus, DeviceStatus::getGridVoltage);
+        registerGauge(inverter, "easun_grid_frequency", deviceStatus, DeviceStatus::getGridFrequency);
+        registerGauge(inverter, "easun_output_voltage", deviceStatus, DeviceStatus::getOutputVoltage);
+        registerGauge(inverter, "easun_output_frequency", deviceStatus, DeviceStatus::getOutputFrequency);
+        registerGauge(inverter, "easun_output_apparent_power", deviceStatus, DeviceStatus::getOutputApparentPower);
+        registerGauge(inverter, "easun_output_active_power", deviceStatus, DeviceStatus::getOutputActivePower);
+        registerGauge(inverter, "easun_bus_voltage", deviceStatus, DeviceStatus::getBusVoltage);
+        registerGauge(inverter, "easun_output_load_percent", deviceStatus, DeviceStatus::getOutputLoadPercent);
+        registerGauge(inverter, "easun_battery_voltage", deviceStatus, DeviceStatus::getBatteryVoltage);
+        registerGauge(inverter, "easun_battery_charge_current", deviceStatus, DeviceStatus::getBatteryChargeCurrent);
+        registerGauge(inverter, "easun_battery_state_of_charge", deviceStatus, DeviceStatus::getBatteryStateOfCharge);
+        registerGauge(inverter, "easun_inverter_heat_sink_temp", deviceStatus, DeviceStatus::getInverterHeatSinkTemperature);
+        registerGauge(inverter, "easun_solar_input_current", deviceStatus, DeviceStatus::getSolarInputCurrent);
+        registerGauge(inverter, "easun_solar_input_voltage", deviceStatus, DeviceStatus::getSolarInputVoltage);
+        registerGauge(inverter, "easun_battery_voltage_scc", deviceStatus, DeviceStatus::getBatteryVoltageScc);
+        registerGauge(inverter, "easun_battery_discharge_current", deviceStatus, DeviceStatus::getBatteryDischargeCurrent);
 
     }
 
