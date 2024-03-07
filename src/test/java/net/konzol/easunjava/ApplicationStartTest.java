fbd659ed68ed05371d37446980708784d442306e
Donath Peter <donath.peter@gmail.com>
2024-03-07 14:40:32 +0100

    extended pom with native reqirements

---
+++ src/test/java/net/konzol/easunjava/ApplicationStartTest.java
@@ -0,0 +1,13 @@
+package net.konzol.easunjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationStartTest {
+
+    @Test
+    public void applicationStartTest() {
+
+    }
+}
